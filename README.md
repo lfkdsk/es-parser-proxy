ad97b50302d0dc050ecdb49659d08897bfb4d3b8
lfkdsk <lfk_dsk@hotmail.com>
2018-01-22 01:33:39 +0800

    更新了文档，好累啊

--- README.md
+++ README.md
@@ -33,7 +33,7 @@
     "disableHighlight": false
   }
   ```
-## AST Structure 
+## AST 结构 
 
 ``` javascript
 // AstObject -> Property List
@@ -52,4 +52,120 @@
   }
 }
 ```
+
+## 语法定义
+
+可以通过注解的方式为解析器增加语法，增加的语法是一种类似的 JSON 语法：
+
+``` java
+    @Bind(name = "match_all", mode = OBJECT, prefix = "query")
+    public void matchAll(AstObjectProperty property, Context context) {
+
+    }
+```
+
+* `name` 表示当前添加的语法描述。
+* `mode` 表示添加的语法类型，语法类型包含四种，`[ WRAPPER, OBJECT, PRIMARY, ARRAY] ` 其中 Wrapper 代表最外层对象，Object 代表添加的是一个对象类型，Primary 代表字面量类型，Array 代表数组类型。
+* `prefix` 代表语法前缀，当前的 node 在哪个对象之下。
+* `insert` 代表插入的语法规则，适用于递归的语法定义（目前用于嵌套的 Query Context）
+
+### 示例 1
+
+比如我们需要支持语法：
+
+``` javascript
+{
+  "query": {
+    "match_all": {}
+  }
+}
+```
+
+可以编写下列的语法规则：
+
+``` java
+@Bind(name = "wrapper", mode = WRAPPER)
+public void wrapper(AstQueryProgram property, Context context) {
+    Logger.v(" wrapper attach ");
+}
+
+@Bind(name = "query", mode = OBJECT, prefix = "wrapper")
+public void query(AstObjectProperty property, Context context) {
+    Logger.v(" query attach ");
+}
+
+@Bind(name = "match_all", mode = OBJECT, prefix = "query")
+public void matchAll(AstObjectProperty property, Context context) {
+    Logger.v(" match_all attach ");
+}
+```
+
+打印的输出结果：
+
+![match_all](art/invoke.png)
+
+
+
+### 示例 2
+
+这里出现了递归，`must` 后面的 Object 实际上支持 `query` 的语法：
+
+``` javascript
+{
+  "query": {
+    "bool": {
+      "must" : {
+        "term" : { "user" : "kimchy" }
+      },
+      "filter": {
+        "term" : { "tag" : "tech" }
+      },
+    }
+  }
+}
+```
+
+可以编写下列的语法规则：
+
+``` java
+@Bind(name = "wrapper", mode = WRAPPER)
+public void wrapper(AstQueryProgram property, Context context) {
+    System.out.println(" Call Wrapper ");
+}
+
+@Bind(name = "query", mode = OBJECT, prefix = "wrapper")
+public void query(AstObjectProperty property, Context context) {
+    System.out.println(" Call Query ");
+}
+
+@Bind(name = "term", mode = OBJECT, prefix = "query")
+public void term(AstObjectProperty property, Context context) {
+    System.out.println(" Call Term ");
+}
+
+@Bind(name = "bool", mode = OBJECT, prefix = "query")
+public void bool(AstObjectProperty property, Context context) {
+    System.out.println(" Call Bool ");
+}
+
+@Bind(name = "must", mode = OBJECT, prefix = "bool", insert = "query")
+public void must(AstObjectProperty property, Context context) {
+    System.out.println(property.object()
+                               .property("term")
+                               .valueNode()
+                               .toString());
+}
+
+@Bind(name = "filter", mode = OBJECT, prefix = "bool", insert = "query")
+public void filter(AstObjectProperty property, Context context) {
+    System.out.println(property.object()
+                               .property("term")
+                               .valueNode()
+                               .toString());
+}
+```
+
+打印的输出结果：
+
+![re](art/re.png)
 
--- src/test/java/dashbase/ast/property/PropertyTest.java
+++ src/test/java/dashbase/ast/property/PropertyTest.java
@@ -1,9 +1,17 @@
 package dashbase.ast.property;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import dashbase.ast.AstQueryProgram;
 import dashbase.ast.object.AstObjectProperty;
+import dashbase.bnf.BnfCom;
 import dashbase.env.Context;
+import dashbase.lexer.JustLexer;
 import dashbase.meta.Bind;
+import dashbase.meta.BnfGenerator;
+import dashbase.utils.GrammarHelper;
+import dashbase.utils.logger.Logger;
+import org.junit.Test;
 
 import static dashbase.meta.GrammarMode.OBJECT;
 import static dashbase.meta.GrammarMode.WRAPPER;
@@ -12,22 +20,22 @@ public class PropertyTest {
 
     @Bind(name = "wrapper", mode = WRAPPER)
     public void wrapper(AstQueryProgram property, Context context) {
-
+        Logger.v(" wrapper attach ");
     }
 
     @Bind(name = "query", mode = OBJECT, prefix = "wrapper")
     public void query(AstObjectProperty property, Context context) {
-
+        Logger.v(" query attach ");
     }
 
     @Bind(name = "match_all", mode = OBJECT, prefix = "query")
     public void matchAll(AstObjectProperty property, Context context) {
-
+        Logger.v(" match_all attach ");
     }
 
     @Bind(name = "match", mode = OBJECT, prefix = "query")
     public void match(AstObjectProperty property, Context context) {
-
+        System.out.println(" match ");
     }
 
     @Bind(name = "match_phrase", mode = OBJECT, prefix = "query")
@@ -95,5 +103,97 @@ public class PropertyTest {
 
     }
 
+    public static class ProxyDemo {
+        @Bind(name = "wrapper", mode = WRAPPER)
+        public void wrapper(AstQueryProgram property, Context context) {
+            System.out.println(" Call Wrapper ");
+        }
 
+        @Bind(name = "query", mode = OBJECT, prefix = "wrapper")
+        public void query(AstObjectProperty property, Context context) {
+            System.out.println(" Call Query ");
+        }
+
+        @Bind(name = "term", mode = OBJECT, prefix = "query")
+        public void term(AstObjectProperty property, Context context) {
+            System.out.println(" Call Term ");
+        }
+
+        @Bind(name = "bool", mode = OBJECT, prefix = "query")
+        public void bool(AstObjectProperty property, Context context) {
+            System.out.println(" Call Bool ");
+        }
+
+        @Bind(name = "must", mode = OBJECT, prefix = "bool", insert = "query")
+        public void must(AstObjectProperty property, Context context) {
+            System.out.println(property.object()
+                                       .property("term")
+                                       .valueNode()
+                                       .toString());
+        }
+
+        @Bind(name = "filter", mode = OBJECT, prefix = "bool", insert = "query")
+        public void filter(AstObjectProperty property, Context context) {
+            System.out.println(property.object()
+                                       .property("term")
+                                       .valueNode()
+                                       .toString());
+        }
+    }
+
+    @Test
+    public void testProperty() {
+        BnfGenerator generator = new BnfGenerator();
+        generator.register(getClass());
+        Logger.init();
+
+        JsonObject wrapper = new JsonObject();
+        JsonObject query = new JsonObject();
+        JsonObject match_all = new JsonObject();
+
+        query.add("match_all", match_all);
+        wrapper.add("query", query);
+
+        Logger.v(new GsonBuilder().setPrettyPrinting().create().toJson(wrapper));
+        JustLexer lexer = new JustLexer(wrapper.toString());
+        lexer.reserved("query");
+        lexer.reserved("match_all");
+
+        BnfCom bnfCom = generator.generate();
+        AstQueryProgram program = (AstQueryProgram) GrammarHelper.transformAst(bnfCom.parse(lexer));
+        program.eval(generator.context());
+    }
+
+    @Test
+    public void testProperty1() {
+        BnfGenerator generator = new BnfGenerator();
+        generator.register(ProxyDemo.class);
+        Logger.init();
+
+        String demo = "{\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"must\" : {\n" +
+                "        \"term\" : { \"user\" : \"kimchy\" }\n" +
+                "      },\n" +
+                "      \"filter\": {\n" +
+                "        \"term\" : { \"tag\" : \"tech\" }\n" +
+                "      },\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+        Logger.v(demo);
+        JustLexer lexer = new JustLexer(demo);
+        lexer.reserved("query");
+        lexer.reserved("match_all");
+        lexer.reserved("must");
+        lexer.reserved("bool");
+        lexer.reserved("term");
+        lexer.reserved("filter");
+
+        BnfCom bnfCom = generator.generate();
+        AstQueryProgram program = (AstQueryProgram) GrammarHelper.transformAst(bnfCom.parse(lexer));
+        program.eval(generator.context());
+    }
 }
